188ee6a81884c11e32263b17c487093bf0d12895
vikram-gsu <vpareddy1@student.gsu.edu>
2017-03-06 21:02:23 -0500

    Components and Modules in folder structure

---
+++ src/ShoppingCart/WeightLossFilter.service.js
@@ -0,0 +1,29 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('ShoppingList')
+        .service('WeightLossFilterService', WeightLossFilterService)
+
+    WeightLossFilterService.$inject = ['$q', '$timeout'];
+
+    function WeightLossFilterService($q, $timeout) {
+        var service = this
+
+        this.checkName = (name) => {
+            var deferred = $q.defer()
+                let result = {message: ''}
+                $timeout(() => {
+                    if(name.toLowerCase().indexOf('cookie') != -1){
+                        result.message = 'No more cookies for you' 
+                        deferred.reject(result)
+                    }else{
+                        deferred.resolve(result)
+                    }
+                }, 900)
+            
+            
+            return deferred.promise
+        }
+    }
+})();
